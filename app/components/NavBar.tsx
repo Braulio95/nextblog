705853287dafe526ebcd9b5f235d2c1dd6be1ec7
Braulio95 <braulioestradaalfonso@hotmail.com>
2023-09-29 10:44:41 -0600

    main config of the project displayed

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import { FaYoutube, FaGithub, FaLinkedin } from "react-icons/fa";
+
+export default function NavBar() {
+  return (
+    <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
+      <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
+        <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
+          <Link
+            href="/"
+            className="text-white/90 no-underline hover:text-white"
+          >
+            Braulio
+          </Link>
+        </h1>
+        <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl">
+          <Link
+            className="text-white/90 hover:text-white"
+            href="https://www.youtube.com/@sixstringman2862"
+          >
+            <FaYoutube />
+          </Link>
+
+          <Link
+            className="text-white/90 hover:text-white"
+            href="https://github.com/Braulio95"
+          >
+            <FaGithub />
+          </Link>
+          <Link
+            className="text-white/90 hover:text-white"
+            href="https://www.linkedin.com/in/braulioestradaalfonso/"
+          >
+            <FaLinkedin />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
